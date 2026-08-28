2593eb45caa4a7744abbb07192ca1b6539fb0328
Dexter <josembotero17qgmail.com>
2026-08-27 20:07:04 -0500

    Variables base

---
+++ Catalogo.java
@@ -0,0 +1,14 @@
+public class Catalogo {
+    public static void main(String[] args) {
+        //Constantes. 
+        final double PRECIO_BASICO = 15000.0;
+        final double PRECIO_MEDIO = 35000.0;
+        final double PRECIO_PREMIUM = 60000.0;
+        final double TASA_IVA = 0.19;
+        //Variables.
+        double totalBruto = 0.0;
+        int totalArticulos = 0;
+        int cantidadPremium = 0;
+        int opcionMenu = 0;
+        Sysout.println("Catalogo de productos");
+    }
