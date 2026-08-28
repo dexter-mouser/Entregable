1fef70d4d61ba046236ee9cb10c32fc6e6103357
Dexter <josembotero17qgmail.com>
2026-08-27 21:30:27 -0500

    Variables INVENTARIO

---
+++ inventario.java
@@ -0,0 +1,14 @@
+public class Inventario {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Variables
+        int cantidadProductosLote = 0;
+        String nombreProducto = "";
+        double costoBase = 0.0;
+        //Acumuladores
+        double costoTotalAcumulado = 0.0;
+        int contadorCategoriaAlta = 0;
+        double costoPromedio = 0.0;
+        //
+        System.out.println("--Auditoria de Inventario--");
+    }
